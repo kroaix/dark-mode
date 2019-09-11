0ebfb35a1f413cce998df54497e7bf2f3878f169
Katrina Roaix <kroaix@gmail.com>
2019-09-10 23:05:40 -0400

    MVP Complete

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -2,13 +2,13 @@ import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = () => {
-  const [darkMode, enableDarkMode] = useLocalStorage("dark", false);
+  const [darkMode, SetDarkMode] = useLocalStorage("dark", false);
 
   useEffect(() => {
     const body = window.document.body;
     darkMode ? body.classList.add('dark-mode') : body.classList.remove('dark-mode');
   }, [darkMode]);
-  return [darkMode, enableDarkMode];
+  return [darkMode, SetDarkMode];
 };
 
 export default useDarkMode;
